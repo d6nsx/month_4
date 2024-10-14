0184aa150886fd65be08341cce5e1f12abb2c7d7
d6nsx <144886207+d6nsx@users.noreply.github.com>
2024-10-14 18:28:32 +0600

    Add files via upload

---
+++ lesson1/src/components/Compot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Compot = ({compot}) => {
+    return (
+        <div>
+            <p>compot: {compot}</p>
+        </div>
+    );
+};
+
+export default Compot;
---
+++ lesson1/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Title } from './Title';
+
+
+const Footer = () => {
+    return (
+        <div>
+            <Title/>
+        </div>
+    );
+};
+export const Footer2 = () => {
+    return (
+        <div>
+            <Title title={'Footer'}/>
+        </div>
+    );
+};
+
+export default Footer;
